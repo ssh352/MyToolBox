a4730f3ccec87f41c7cff8c57942422054467513
Yang Xiong <yang.xiong@truepartner.cn>
2012-12-05 18:07:11 +0800

    -PriceNumber
    Signed-off-by: Yang Xiong <yang.xiong@truepartner.cn>

--- MyToolBox/PriceNumber.cpp
+++ MyToolBox/PriceNumber.cpp
@@ -1,9 +1,71 @@
-#include "PriceNumber.h"
+﻿#include "PriceNumber.h"
 
-//PriceNumber::PriceNumber(void)
-//{
-//}
-//
-//PriceNumber::~PriceNumber(void)
-//{
-//}
+namespace MY_TOOL_BOX
+{
+	PriceNumber::PriceNumber(void)
+		: m_PriceBase(0),
+		m_PriceExp(0),
+		m_PricePower(10)
+	{
+	}
+
+	PriceNumber::PriceNumber( const PriceNumber& aRef )
+	{
+		m_PriceBase = aRef.m_PriceBase;
+		m_PriceExp = aRef.m_PriceExp;
+		m_PricePower = aRef.m_PricePower;
+	}
+
+	PriceNumber::PriceNumber( int32_t abase,int8_t aExp /*= 0*/, int8_t a_Power /*= 10*/ )
+	{
+		m_PriceBase = abase;
+		m_PriceExp = aExp;
+		m_PricePower = a_Power;
+
+	}
+
+
+	PriceNumber::PriceNumber( double aVal )
+	{
+		m_PriceBase = 0;
+		m_PriceExp = 0;
+		m_PricePower = 10;
+
+		const double MinValDetected = 0.000001;
+		m_PriceBase = static_cast<int32_t>(aVal);
+		double lDoublePart = aVal - m_PriceBase;
+
+		while( abs(lDoublePart) > MinValDetected)
+		{
+			m_PriceBase *= 10;
+			m_PriceExp -= 1;
+			int32_t lInt   = int32_t( (lDoublePart *10) );
+			m_PriceBase += lInt;
+			lDoublePart = lDoublePart *10 - lInt;
+
+		}
+
+		if(abs(aVal - double(*this)) > MinValDetected)
+		{
+			throw std::runtime_error("PriceNumber Convert From double Error");
+		}
+	}
+
+	PriceNumber::operator int32_t() const 
+	{
+		return static_cast<int32_t>(double(*this));
+	}
+
+	PriceNumber::operator double() const 
+	{
+		return m_PriceBase* pow(double(m_PricePower),m_PriceExp);
+	}
+
+	PriceNumber::~PriceNumber(void)
+	{
+
+	}
+
+}
+
+
--- MyToolBox/PriceNumber.h
+++ MyToolBox/PriceNumber.h
@@ -2,21 +2,36 @@
 
 #include<boost\cstdint.hpp>
 #include <boost\operators.hpp>
+#include <boost\integer_traits.hpp>
+#include <exception>
+namespace MY_TOOL_BOX
+{
+	class PriceNumber : boost::ordered_field_operators<PriceNumber,
+						boost::ordered_field_operators<PriceNumber,int,
+						boost::ordered_field_operators<PriceNumber,double> > > 
+	{
+	public:
 
-//class PirceNumber :boost::totally_ordered<PirceNumber>
-//{
-//public:
-//
-//	PirceNumber(void);
-//	~PirceNumber(void);
-//
-//	PirceNumber(const PirceNumber& aRef);
-//	PirceNumber(int32_t abase,int8_t aExp = 0, int8_t a_Power = 10);
-//
-//public:
-//	int32_t m_PriceBase;
-//	int8_t	m_PriceExp;
-//	int8_t	m_PricePower;
-//
-//};
+		PriceNumber(void);
+		~PriceNumber(void);
+
+		PriceNumber(const PriceNumber& aRef);
+
+		explicit PriceNumber(int32_t abase,int8_t aExp = 0, int8_t a_Power = 10);
+		explicit PriceNumber(double aVal)	throw (std::runtime_error);
+
+		operator int32_t() const;
+		operator double() const;
+
+	private:
+		int32_t m_PriceBase;
+		int8_t	m_PriceExp;
+		int8_t	m_PricePower;
+
+		static const int32_t __MAX_VAL_BEFORE_SHIFT_10 = boost::integer_traits<int32_t>::const_max / 10;
+
+	};
+}
+
+
 

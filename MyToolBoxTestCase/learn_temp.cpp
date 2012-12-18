9e16f2e0a567877e2a8cc129d171adcf3bf04cb3
Yang Xiong <yang.xiong@truepartner.cn>
2012-12-18 09:42:37 +0800

    Learn_boost_format
    Signed-off-by: Yang Xiong <yang.xiong@truepartner.cn>

--- MyToolBoxTestCase/learn_temp.cpp
+++ MyToolBoxTestCase/learn_temp.cpp
@@ -1,1 +1,17 @@
 #include "stdafx.h"
+
+#include <boost\format.hpp>
+
+#include <boost\format.hpp>
+
+
+
+
+TEST(boost_format,simpale)
+{
+	int val = 1234;
+	boost::format fmt("%1%");
+	fmt % boost::io::group(std::setfill('0'),std::setw(12),val);
+	std::string lRet = str(fmt);
+	EXPECT_EQ(std::string("000000001234"),lRet);
+}

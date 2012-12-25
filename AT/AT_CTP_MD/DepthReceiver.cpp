3832c382cc3ecdf243b089b1d41f3afd66d9c6c4
Xiong Yang <snake.xiongyang@qq.com>
2012-12-25 23:41:21 +0800

    -partly work
    Signed-off-by: Xiong Yang <snake.xiongyang@qq.com>

--- AT/AT_CTP_MD/DepthReceiver.cpp
+++ AT/AT_CTP_MD/DepthReceiver.cpp
@@ -16,7 +16,13 @@ namespace CTP
 
 	void DepthReceiver::Start()
 	{
-		
+		char lConPath[128] = "./MD_DepthReceiver/";
+		m_pMDAPI = CThostFtdcTraderApi::CreateFtdcTraderApi();
+		m_pMDAPI->RegisterSpi(this);		 
+		m_pMDAPI->RegisterFront("tcp://asp-sim2-front1.financial-trading-platform.com:26205");
+		m_pMDAPI->Init();
+		m_RuningState = CONNECTING_STATE;
+		m_pCTP_MD->NotifySubModuleState(CTP_MD_StateReceiver_Connecting);	
 	}
 
 }

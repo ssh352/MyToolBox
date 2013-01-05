af9b9196f9c19b9b9657c8ac28183a80dbe265fe
Xiong Yang <snake.xiongyang@qq.com>
2013-01-05 19:40:49 +0800

    -start TD
    Signed-off-by: Xiong Yang <snake.xiongyang@qq.com>

--- AT/AT_Driver/IDriver_TD.h
+++ AT/AT_Driver/IDriver_TD.h
@@ -13,12 +13,12 @@ namespace AT
 	{
 
 	public:
-		virtual Init(const std::map<std::string,std::string>& aConfigMap,ITradeSpi* apTradeSpi);
+		virtual void Init(const std::map<std::string,std::string>& aConfigMap,ITradeSpi* apTradeSpi) =0;
 	public:
-		virtual std::string CreateOrder(const std::string& aNewOrder);
-		virtual void DeleteOrder(const std::string& aClientOrderID);
-		virtual	void ModifyOrder(const std::string& aRequest);
-		virtual void QueryPosition(const std::string& aRequest);
+		virtual std::string CreateOrder(const std::string& aNewOrder) =0;
+		virtual void DeleteOrder(const std::string& aClientOrderID) =0;
+		virtual	void ModifyOrder(const std::string& aRequest) =0;
+		virtual void QueryPosition(const std::string& aRequest) =0;
 
 	};
 

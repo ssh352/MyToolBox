0cdc0b16d1f6fdf2e79aed7ffe959e8dc8209700
Yang Xiong <yang.xiong@truepartner.cn>
2013-01-14 11:05:11 +0800

    learn more about asio
    Signed-off-by: Yang Xiong <yang.xiong@truepartner.cn>

--- MyToolBoxTestCase/asioTestFile.cpp
+++ MyToolBoxTestCase/asioTestFile.cpp
@@ -4,135 +4,8 @@
 #include <boost/date_time/posix_time/posix_time.hpp>
 #include <boost/thread.hpp>
 #include <boost\bind.hpp>
-
-class printer;
-
-class DISABLED_asioTestFix  :public ::testing::Test
-{
-public:
-	virtual void SetUp()
-	{
-		m_pwork = new boost::asio::io_service::work(io);
-		m_pstrand = new boost::asio::strand(io);
-		m_pThread1.swap(boost::thread(boost::bind(&boost::asio::io_service::run,&io) ));
-		m_pThread2.swap(boost::thread(boost::bind(&boost::asio::io_service::run,&io) ));
-		m_pThread3.swap(boost::thread(boost::bind(&boost::asio::io_service::run,&io) ));
-		m_pThread4.swap(boost::thread(boost::bind(&boost::asio::io_service::run,&io) ));
-		m_pPrinter = NULL;
-	}
-
-	virtual void TearDown()
-	{
-		delete m_pwork;
-		m_pThread1.join();
-		m_pThread2.join();
-		m_pThread3.join();
-		m_pThread4.join();
-		delete m_pstrand;
-		if(m_pPrinter!= NULL)
-		{
-			delete m_pPrinter;
-			m_pPrinter = NULL;
-		}
-	}
-
-	 static void SetUpTestCase()
-	{
-
-
-	}
-
-	 static void TearDownTestCase()
-	{
-
-	}
-
-	 boost::asio::io_service io;
-	 boost::asio::io_service::work* m_pwork;
-	 boost::mutex m_lock;
-
-	 boost::asio::strand* m_pstrand;
-
-	 boost::thread m_pThread1;
-	 boost::thread m_pThread2;
-	 boost::thread m_pThread3;
-	 boost::thread m_pThread4;
-
-
-	 printer* m_pPrinter;
-
-	//test function
-public:
-
-};
-
-
-void print(const boost::system::error_code& /*e*/)
-{
-	boost::mutex::scoped_lock(stdIOlock);
-	std::cout<<"Call Thread "<<boost::this_thread::get_id();
-	std::cout << "\nHello, world! The sync Print\n";
-}
-
-
-
-TEST_F(DISABLED_asioTestFix,timer2)
-{
-	boost::asio::deadline_timer t(io, boost::posix_time::seconds(5));
-	t.async_wait(&print);
-}
-
-//
-void print(const boost::system::error_code& /*e*/,
-	boost::asio::deadline_timer* t, int* count)
-{
-	
-	if (*count < 2)
-	{
-		boost::mutex::scoped_lock(stdIOlock);
-		std::cout<<"Call Thread "<<boost::this_thread::get_id();
-		std::cout <<" "<< *count << "\n";
-		++(*count);
-
-		t->expires_at(t->expires_at() + boost::posix_time::seconds(1));
-		t->async_wait(boost::bind(print,
-			boost::asio::placeholders::error, t, count));
-	}
-	else
-	{
-		delete t;
-		delete count;
-	}
-}
-
-TEST_F(DISABLED_asioTestFix,timer3)
-{
-	int* lpcount = new int(0);
-	boost::asio::deadline_timer* lpTimer = new boost::asio::deadline_timer(io, boost::posix_time::seconds(1));
-	lpTimer->async_wait(boost::bind(print,boost::asio::placeholders::error,lpTimer, lpcount));
-}
-
-
-void wasterTime()
-{
-	boost::this_thread::sleep(boost::posix_time::seconds(0));
-	{
-		std::cout<<"wasterTime Call Thread "<<boost::this_thread::get_id()<<std::endl;
-	}
-
-
-}
-
-
-TEST_F(DISABLED_asioTestFix,conCAll)
-{
-
-	for(int i=0;i<2;++i)
-	{
-		io.post(m_pstrand->wrap(boost::bind(wasterTime)));
-		//io.wrap((boost::bind(wasterTime)));
-	}
-}
+#include <boost\shared_ptr.hpp>
+#include <boost\foreach.hpp>
 
 
 class printer
@@ -170,7 +43,7 @@ public:
 	void print2()
 	{
 		boost::mutex::scoped_lock lock1(m_lock);
-		
+
 
 		std::cout<<"print2 Call Thread "<<boost::this_thread::get_id();
 		if (count_ < 10)
@@ -191,7 +64,150 @@ private:
 	boost::mutex m_lock;
 };
 
-TEST_F(DISABLED_asioTestFix,Printer)
+
+class asioTestFix  :public ::testing::Test
+{
+public:
+	virtual void SetUp()
+	{
+		m_pwork = new boost::asio::io_service::work(io);
+		m_pstrand = new boost::asio::strand(io);
+		m_ThreadCount = 0;
+	
+		m_pPrinter = NULL;
+	}
+
+	virtual void TearDown()
+	{
+
+		delete m_pwork;
+		delete m_pstrand;
+
+		BOOST_FOREACH(auto lpThread,m_ThreadPool)
+		{
+			lpThread->join();
+		}
+
+
+		if(m_pPrinter!= NULL)
+		{
+			delete m_pPrinter;
+			m_pPrinter = NULL;
+		}
+	}
+
+	void InitThreadPool()
+	{
+		for(int i =0 ; i< m_ThreadCount; ++i)
+		{
+			boost::shared_ptr<boost::thread > lpThread(new boost::thread(boost::bind(&boost::asio::io_service::run,&io) ));
+			m_ThreadPool.push_back(lpThread);
+		}
+	}
+
+	 static void SetUpTestCase()
+	{
+
+
+	}
+
+	 static void TearDownTestCase()
+	{
+
+	}
+
+	 boost::asio::io_service io;
+	 boost::asio::io_service::work* m_pwork;
+	 boost::mutex m_lock;
+	 boost::asio::strand* m_pstrand;
+
+
+	 int		m_ThreadCount;
+	 std::vector<boost::shared_ptr<boost::thread> > m_ThreadPool;
+
+
+	 printer* m_pPrinter;
+	//test function
+public:
+
+};
+
+boost::mutex g_STDIOlock;
+
+void print(const boost::system::error_code& , boost::shared_ptr< boost::asio::deadline_timer >  apTimer)
+{
+	//boost::mutex::scoped_lock lock(g_STDIOlock);
+	std::cout<<"Call Thread "<<boost::this_thread::get_id()<<"\n";
+	std::cout << "Hello, world! The async Print timer2 \n";
+}
+
+
+
+TEST_F(asioTestFix,timer2)
+{
+	m_ThreadCount = 1;
+	InitThreadPool();
+	 boost::shared_ptr< boost::asio::deadline_timer > lpTimer(new boost::asio::deadline_timer(io, boost::posix_time::seconds(5)));
+	lpTimer->async_wait(boost::bind(&print,boost::asio::placeholders::error,lpTimer));
+}
+
+//
+void print(const boost::system::error_code& /*e*/,
+	boost::asio::deadline_timer* t, int* count)
+{
+	
+	if (*count < 2)
+	{
+		//boost::mutex::scoped_lock lock(g_STDIOlock);
+		std::cout<<"Call Thread "<<boost::this_thread::get_id();
+		std::cout <<" "<< *count << "\n";
+		++(*count);
+
+		t->expires_at(t->expires_at() + boost::posix_time::seconds(1));
+		t->async_wait(boost::bind(print,
+			boost::asio::placeholders::error, t, count));
+	}
+	else
+	{
+		delete t;
+		delete count;
+	}
+}
+
+TEST_F(asioTestFix,timer3)
+{
+	m_ThreadCount = 1;
+	InitThreadPool();
+
+	int* lpcount = new int(0);
+	boost::asio::deadline_timer* lpTimer = new boost::asio::deadline_timer(io, boost::posix_time::seconds(1));
+	lpTimer->async_wait(boost::bind(print,boost::asio::placeholders::error,lpTimer, lpcount));
+}
+
+
+void wasterTime()
+{
+	boost::this_thread::sleep(boost::posix_time::seconds(0));
+	{
+		std::cout<<"wasterTime Call Thread "<<boost::this_thread::get_id()<<std::endl;
+	}
+
+
+}
+
+
+TEST_F(asioTestFix,conCAll)
+{
+
+	for(int i=0;i<2;++i)
+	{
+		io.post(m_pstrand->wrap(boost::bind(wasterTime)));
+		//io.wrap((boost::bind(wasterTime)));
+	}
+}
+
+
+TEST_F(asioTestFix,Printer)
 {
 	//boost::asio::io_service io;
 	
--- TempTest/main.cpp
+++ TempTest/main.cpp
@@ -5,26 +5,52 @@
 using namespace std;
 #include <boost\foreach.hpp>
 #include <map>
+
+
+class TestA
+{
+	public:
+	TestA(int * ap)
+		:m_pInt(ap)
+	{
+
+	}
+	int* m_pInt;
+};
+class TestB :public TestA
+{
+	public:
+	TestB()
+		:TestA(&m_autoPint)
+		, m_autoPint(123)
+		
+	{
+
+	}
+	int m_autoPint;
+};
 int main()
 {
 
-
-	string test ("xxxxxx" );
-	const char * test_data_ptr = test.data();
-
-
-	const char* lcBUf = "xxxxxxx";
-	string test2 (lcBUf,6);
-	const char * test_data_ptr2 = test2.data();
-	//const char * test_cptr = test.c_str();
+	TestB inst;
+	//string test ("xxxxxx" );
+	//const char * test_data_ptr = test.data();
 
 
-	std::map<string,int> testMap;
-	typedef std::map<string,int>::_Val_type mapVal;
-	BOOST_FOREACH(mapVal lval,testMap)
-	{
-		std::cout<< lval.second;
-	}
-	
+	//const char* lcBUf = "xxxxxxx";
+	//string test2 (lcBUf,6);
+	//const char * test_data_ptr2 = test2.data();
+	////const char * test_cptr = test.c_str();
+
+
+	//std::map<string,int> testMap;
+	//typedef std::map<string,int>::_Val_type mapVal;
+	//BOOST_FOREACH(auto lval,testMap)
+	//{
+	//	std::cout<< lval.second;
+	//}
+	return 0;
+
+
 
 }

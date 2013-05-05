83f70aa1682fd9bd76edce7c8fa620f0899ab86c
xiongyang <snake.xiongyang@qq.com>
2013-05-06 02:53:12 +0800

    small clean
    Signed-off-by: xiongyang <snake.xiongyang@qq.com>

--- AT/AT_CTP_MD_DataReplayer/misc.cpp
+++ AT/AT_CTP_MD_DataReplayer/misc.cpp
@@ -1,6 +1,6 @@
 #include "CTP_MD_Replayer.h"
 
-DLL_API AT::IDriver_MD* CreateMD(const char*  aConfig, AT::IMarketSpi* apMarketSpi)
+DLL_API AT::IDriver_MD* CreateMD(const char*  aMarketDBPath, AT::IMarketSpi* apMarketSpi)
 {
-	return new CTP::CTP_MD_Replayer(aConfig,apMarketSpi);
+	return new CTP::CTP_MD_Replayer(aMarketDBPath,apMarketSpi);
 }

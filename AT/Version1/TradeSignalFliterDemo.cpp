a4bb18cf9cf2b85548cfcaac2ab979699441d0d4
xiongyang <snake.xiongyang@qq.com>
2013-06-24 05:43:15 +0800

    -for filter

--- AT/Version1/TradeSignalFliterDemo.cpp
+++ AT/Version1/TradeSignalFliterDemo.cpp
@@ -6,6 +6,14 @@ namespace AT
 
 TradeSignalFliterDemo::TradeSignalFliterDemo(void)
 {
+	//load from  files
+	m_StopTriggerVol2;
+	m_StopTriggerVol3;
+
+	m_Time1;
+	m_Time2;
+	m_Time3;
+	m_TotalProfitStopVal;
 }
 
 
@@ -30,13 +38,33 @@ TradeSignal TradeSignalFliterDemo::FliterTradeSignal( std::vector<TradeSignal> a
 	}
 
 	//连续亏损1次
-	if(m_ProfitStatusMap.size() >= 1 && m_ProfitStatusMap.rbegin()->second < 0 && (m_LastTime - m_ProfitStatusMap.rbegin()->first)  > boost::posix_time::seconds(m_Time1) )
+
+	LastNTradeStatus lret = IsLastNTradeLoss(1);
+
+	if(lret.m_isAllLoss && (m_LastTime - m_ProfitStatusMap.rbegin()->first) < boost::posix_time::seconds(m_Time1) )
 	{
 		TradeSignal lret;
 		lret.m_Valid = false;
 		return lret;
 	}
 	//2次
+
+	LastNTradeStatus lret2 = IsLastNTradeLoss(2);
+	if(lret2.m_isAllLoss && lret2.m_TotalLoss > m_StopTriggerVol2 )
+	{
+		TradeSignal lret;
+		lret.m_Valid = false;
+		return lret;
+	}
+
+
+	LastNTradeStatus lret3 = IsLastNTradeLoss(3);
+	if(lret3.m_isAllLoss && lret3.m_TotalLoss > m_StopTriggerVol3 )
+	{
+		TradeSignal lret;
+		lret.m_Valid = false;
+		return lret;
+	}
 	//3次
 
 	//总亏损
@@ -81,6 +109,26 @@ void TradeSignalFliterDemo::UpdateProfit( int32_t aProfit,AT_Time aTime )
 void TradeSignalFliterDemo::OnMarketDepth( const AT::MarketData& aMarket )
 {
 	m_LastTime  = aMarket.m_UpdateTime;
+}
+
+TradeSignalFliterDemo::LastNTradeStatus TradeSignalFliterDemo::IsLastNTradeLoss( int lastI )
+{
+	LastNTradeStatus lret = {false,0};
+	std::map<AT_Time,int32_t>::reverse_iterator iter = m_ProfitStatusMap.rbegin();
+	for (int i = lastI; i< lastI;++i)
+	{
+		if(iter!= m_ProfitStatusMap.rend() && iter->second < 0 )
+		{
+			lret.m_TotalLoss += iter->second;
+		}
+		else
+		{
+			lret.m_isAllLoss = false;
+			return lret;
+		}
+	}
+	lret.m_isAllLoss = true;
+	return lret;
 }
 
 
--- AT/Version1/TradeSignalFliterDemo.h
+++ AT/Version1/TradeSignalFliterDemo.h
@@ -17,6 +17,13 @@ public:
 	virtual void UpdateProfit(int32_t aProfit,AT_Time aTime) override ;
 
 private:
+	struct LastNTradeStatus
+	{
+		bool m_isAllLoss;
+		int	m_TotalLoss;
+	};
+	LastNTradeStatus IsLastNTradeLoss(int i);
+
 
 	bool m_IsOnLastSignal;
 	AT_Time m_LastTime;
@@ -25,6 +32,8 @@ private:
 
 private:
 
+	int m_StopTriggerVol2;
+	int m_StopTriggerVol3;
 
 	int  m_Time1;
 	int	 m_Time2;
